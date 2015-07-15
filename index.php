012af46e54ee72c488735cf20e69df20a86f65fc
Colin Michaels <colin@colinmichaels.com>
2015-07-15 15:21:51 -0400

    removed test code
    removed sql statements

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-
 define('INCLUDE_CHECK',true);
 include_once("name_generator/functions.php"); 
-
-
 
 $load = array();
  $names =names(100);
@@ -22,15 +19,5 @@ foreach($names as $name)
 </td>
 </table>
 <?php 
-
-/*
-
-$sql_insert_table = SITEINT."_orders";
-		$sql_insert_fields = "`id`,`".implode("`, `", $keys)."`, `createDate`, `modDate`";
-		$sql_insert_values = 	"NULL,'".implode("','",$dbValues)."',CURRENT_TIMESTAMP, CURRENT_TIMESTAMP";
-		if(!cm_mysqli_insert($sql_insert_table,$sql_insert_fields,$sql_insert_values)) 
-			die("DB INSERT FAILED <br> PLEASE TRY AGAIN<br>".$sql_insert_values); 
-		
-		*/
 		
 		?>
